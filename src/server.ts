eb1f21d3fb5e8dcfe94904cb45a93fed7125a76d
Satyadev Maurya <in.satyadev@gmail.com>
2024-10-16 22:02:06 +0530

    impliment cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 import dotenv from "dotenv"
 import connectDB  from "./config/connection"
 import UserRoute from './routes/user'
@@ -12,7 +14,15 @@ const port = process.env.PORT;
 
 connectDB();
 
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", UserRoute)
 app.use("/api", TaskRoute)
